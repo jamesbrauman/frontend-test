0f513e046e7de4586b7a2a63b4977973440cc412
James Brauman <james.22au@gmail.com>
2015-12-12 18:00:31 +1100

    Fixed a small bug where text input wasn't being cleared after submission.

--- src/js/components/NewCounterInput.js
+++ src/js/components/NewCounterInput.js
@@ -30,7 +30,7 @@ class NewCounterInput extends React.Component {
                 <form onSubmit={this.handleSubmit}>
                     <label className="sr-only" htmlFor="counter-title">Email address</label>
                     <div className="input-group">
-                        <input type="text" required className="form-control" id="counter-title" placeholder="Enter counter title..." value={this.title} onChange={this.onTitleChange}/>
+                        <input type="text" required className="form-control" id="counter-title" placeholder="Enter counter title..." value={this.state.title} onChange={this.onTitleChange}/>
                             <span className="input-group-btn">
                                 <button className="btn btn-success" type="submit">Add Counter</button>
                             </span>
